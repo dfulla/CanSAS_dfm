417bd49a1037bfe3495f206e6b13c523145c599d
dfulla <fulla75@msn.com>
2015-10-09 10:05:41 +0200

    Delete open_tables_2.py

--- open_tables_2.py
+++
@@ -1,9 +0,0 @@
-import tables
-import os
-
-if os.path.isfile("H2O_100pc_2D_0.051kG.ABS_2.hdf5"):
-    h5file = tables.open_file("H2O_100pc_2D_0.051kG.ABS_2.hdf5", driver="H5FD_CORE")
-    print h5file
-    
-else:
-  print "Could not locate file H2O_100pc_2D_0.051kG.ABS.hdf5"
