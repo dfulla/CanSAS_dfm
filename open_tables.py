0ed1593ba1905347c04ca3ec5b27beb076d8e248
Daniel Fulla Marsa <fulla75@msn.com>
2015-10-21 17:05:37 +0200

    Allow input to read external hdf5 file

--- open_tables.py
+++ open_tables.py
@@ -1,9 +1,30 @@
 import tables
 import os
+import sys
 
-if os.path.isfile("H2O_100pc_2D_0.051kG.ABS.hdf5"):
-    h5file = tables.open_file("H2O_100pc_2D_0.051kG.ABS.hdf5", driver="H5FD_CORE")
-    print h5file
+
+def print_h5_structure(h5):
+
+    if os.path.isfile(h5):
+        h5file = tables.open_file(h5, driver="H5FD_CORE")
+        print h5file
+        h5file.close()
     
-else:
-  print "Could not locate file H2O_100pc_2D_0.051kG.ABS.hdf5"
+    else:
+        print "Could not locate file %s"%h5
+        
+if __name__ == '__main__':
+
+    if len(sys.argv) == 1:
+        h5 = raw_input("Write file to read:")
+        if os.path.isfile(h5):
+            print_h5_structure(h5)
+        else:
+            print "file %s not found"
+    
+    if len(sys.argv) == 2:
+        h5 = sys.argv[1]
+        if os.path.isfile(h5):
+            print_h5_structure(h5)
+        else:
+            print "file %s not found"
