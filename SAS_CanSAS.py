175935c549cc2db7a9fcefc4b629688f3957039b
Daniel Fulla <fulla75@msn.com>
2015-10-28 12:01:43 +0100

    cleaned the script from unnecessary comments

--- SAS_CanSAS.py
+++ SAS_CanSAS.py
@@ -166,24 +166,11 @@ class ConvertCansas(ExampleFile):
                    I_array.append(get_columns(exp_files[i])[2])
                    I_2.append(get_columns(exp_files[i])[2])     # sets the intensity of the two samples into I_2
 
-
-           intensities_array = np.empty([2,128,128])
-           intensities_array[0] = I_2[0]
-           intensities_array[1] = I_2[1]
-
-           I_3D = [M,intensities_array[0],intensities_array[1]]
-           I_3D = np.array(I_3D)
-           
 	   self.createDataSet("Qx", Qx, {"units": "1/A"})
 	   self.createDataSet("Qy", Qy, {"units": "1/A"})
            self.createDataSet("Qz", Qz, {"units": "1/A"})
            self.createDataSet("M",  M,  {"units":  "kG"})
-
-           #self.createDataSet("test",(2,128,128), )
-
-           
-           self.createDataSet("I_array", intensities_array, {"units": "1/cm"}) # intensity from both files
-           #self.createDataSet("I_3D", I_3D, {"units": "1/cm"}) # error gotten (TypeError: Object dtype dtype('O') has no native HDF5 equivalent) 
+           self.createDataSet("I_array", intensities_array, {"units": "1/cm"})
 	   self.closeFile()
         
 def main(exp_files):
